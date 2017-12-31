1cf3b96c90d6182f6881f2c7c00a28d9b254674f
karangoel16 <karangoel16@yahoo.com>
2017-12-31 02:28:24 -0500

    added correct and Incorrect Questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,9 @@
 class Question #< ApplicationRecord
     include Mongoid::Document
     field :name, type: String
-    field :correct, type: String
-    field :notCorrect, type: String
+    field :correctAnswer, type: String, default: "Correct"
+    field :notCorrectAnswer, type: String, default: "Not Correct"
     field :user, type: String
+    field :correct, type: Integer, default: 0
+    field :total, type: Integer, default: 0
 end
